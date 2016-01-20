af534da06051a80e9b5759ae497201f2cc77ae9f
Maxence BUNEL <maxence.bunel@gmail.com>
2016-01-21 00:37:51 +0100

    0

--- Veloader.py
+++ Veloader.py
@@ -1,77 +1,58 @@
 import sqlalchemy
-print (sqlalchemy.__version__ )
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime, Enum, SmallInteger, Numeric
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime,timedelta
-import pandas as panda                ##require python -m pip install pandas
-import numpy as np
-import sys
-import time
-from pandas.core import format
-
-Base = declarative_base()
-
-class User(Base):
-     __tablename__ = 'users'
-
-     id = Column(Integer, primary_key=True)
-     name = Column(String(25))
-     fullname = Column(String(25))
-     password = Column(String(25))
-
-     def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (
-                             self.name, self.fullname, self.password)
+from datetime import datetime
+from geopandas import GeoDataFrame
+import geopandas as gpd
+import urllib
+from Veloader_datastruct import *  ##Entraine la declaration du schema de la table velovmaxence
+import time ##Pour mesure de temps
 
 
-##print(User.__table__ )
+## Methode de remplacement des valeurs vides par -1
+def replaceMissingDataByNegate1(data):
+    try:
+        if not data:
+            return '-1'
+        else:
+            return data
+    except:
+        return data
 
-'''ed_user = User(name='ed', fullname='Ed Jones', password='edspassword')
-print(ed_user.name)
-print(ed_user.password)
-print(str(ed_user.id))'''
+
+
+start_time_global = time.time() ##TIMER
+
+
+#Parametrage SGBD et creation de la table si absente grace a la couche ORM
 engine = create_engine("postgresql://postgres:P@nd0s+@vps234953.ovh.net:5432/velovDB", echo=True)
 Base.metadata.create_all(engine)
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-session = Session()
-##Session.configure(bind=engine)  # once engine is available
 
-'''ed_user = User(name='ed', fullname='Ed Jones', password='edspassword')
-session.add(ed_user)
-our_user = session.query(User).filter_by(name='ed').first()
-print (our_user)
-ed_user is our_user
-print("here")
-session.add_all([User(name='wendy', fullname='Wendy Williams', password='foobar'),User(name='mary', fullname='Mary Contrary', password='xxg527'),User(name='fred', fullname='Fred Flinstone', password='blah')])
-ed_user.password = 'f8s7ccs'
-print(session.dirty)
-print(session.new)
-session.commit()'''
-'''for instance in session.query(User).order_by(User.id):
-    print(instance.name, instance.fullname)'''
-#print(str(session.query(Objectives_aggregations).count()))
+start_time_global = time.time() ##TIMER
+start_time_json_to_file = time.time() ##TIMER
 
-df=panda.DataFrame()
-df=panda.read_json(path_or_buf="https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=30&request=GetFeature&typename=jcd_jcdecaux.jcdvelov&SRSNAME=urn:ogc:def:crs:EPSG::4171")
-iteration=0
+## Recuperation fichier json et ecriture sur le disque
+geojsonfile=urllib.URLopener()
+geojsonfile.retrieve("https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=400&request=GetFeature&typename=jcd_jcdecaux.jcdvelov&SRSNAME=urn:ogc:def:crs:EPSG::4171",'velov.json')
 
-for row in df.get_values():
-    print (row)
+interval_time_json_to_file = time.time() - start_time_json_to_file
 
-#print (df[0].get('geometry'))
-'''
-for occurence in df :
-    print(occurence)
-    print(occurence.get('geometry').get('coordinates')[0])
-    print(occurence.get('geometry').get('coordinates')[1])
-    print(occurence.get('properties').get('last_update'))
+## Lecture fichier json en memoire
 
-print(df)
-print(df.iloc(10))
-print(df.to_panel())'''
+velovGeoDataFrame=gpd.read_file('velov.json')           ##Charge le contenu du Json dans un GeoDataFrame (herite de  et surcharge pandas.DataFrame)
+velovGeoDataFrame['dateinsert']=datetime.today()        ##Ajoute une colonne avec le timestamp actuel
 
+## Nettoyage des donnees en memoire
+velovGeoDataFrame.drop('geometry', axis=1, inplace=True)                    ## Suppression colonne inutile
+velovGeoDataFrame=velovGeoDataFrame.applymap(replaceMissingDataByNegate1)   ## Remplacement des valeurs vides par -1 necessaire a la methode to_sql surchargee
 
+start_time_sql = time.time() ##TIMER
 
+##Insertion en base
+velovGeoDataFrame.to_sql(name='velovmaxence',con=engine,if_exists='append',dtype=velovTableColumnTypes)
 
+interval_time_sql = time.time() - start_time_sql
+
+interval_global = time.time() - start_time_global ##TIMER
+print ('Total time in seconds:', interval_global) ##TIMER
+print ('temps generation fichier local json:', interval_time_json_to_file) ##TIMER
+print ('temps insertion sql:', interval_time_sql) ##TIMER
